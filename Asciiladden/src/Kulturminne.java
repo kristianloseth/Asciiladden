313f78c392a7aad88f17febadbb692e13a896a99
krist <krist@DESKTOP-HHTQNQF>
2017-10-11 20:35:49 +0200

    Ordnet kulturminneklasse

--- Asciiladden/src/Kulturminne.java
+++ Asciiladden/src/Kulturminne.java
@@ -1,3 +1,39 @@
 public class Kulturminne {
-        
+    private String id = "";
+    private String navn = ""; 
+    private String beskrivelse = "";
+    private String datering = "";
+    
+    public String getId() {
+        return id;        
+    }
+    
+    public void setId(String i) {
+        id = i;
+    } 
+    
+    public String getNavn() {
+        return navn;
+    }
+    
+    public void setNavn(String n) {
+        navn = n;        
+    }
+    
+    public String getBeskrivelse() {
+        return beskrivelse;
+    }
+    
+    public void setBeskrivelse(String b) {
+        beskrivelse = b;
+    }
+    
+    public String getDatering() {
+        return datering;
+    }
+    
+    public void setDatering(String d) {
+        datering = d;
+    }
+    
 }
