a6b0b0b3e1d38eb2f7d28f34dfbf2d891024e8b3
krist <krist@DESKTOP-HHTQNQF>
2017-10-11 20:27:19 +0200

    Fikset spørring.

--- Asciiladden/src/asciiladden/Asciiladden.java
+++ Asciiladden/src/asciiladden/Asciiladden.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package asciiladden;
 
-/**
- *
- * @author Leviathan
- */
 public class Asciiladden {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws Exception {
+        GetMinne minne = new GetMinne(args[0]);
+        String streng = minne.getRespons();
+        System.out.println(streng);
+        minne.getInfo(streng);
     }
     
 }
--- Asciiladden/src/asciiladden/GetMinne.java
+++ Asciiladden/src/asciiladden/GetMinne.java
@@ -1,15 +1,24 @@
 package asciiladden;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.HttpURLConnection;
 
 public class GetMinne {
     
+    private String respons = "";
+    
     public GetMinne(String id) {
         if (id.contains("-")) {
-            getEnkSparql(id);
-        }
-        
+            respons = getEnkSparqlQuery(id);
+        } else {
+            respons = getLokSparqlQuery(id);
+        }        
+    }
+    
+    public String getRespons() {
+        return respons;
     }
     
     public String getLokSparqlQuery(String lokId) {
@@ -30,6 +39,16 @@ public class GetMinne {
                 + "%3E+%3Fp+%3Fo+.%7D%0D%0AOPTIONAL%0D%0A%7B%3Fo+rdfs%3Alabel+%3Flab+.%7D%0D%0A%7D&"
                 + "format=application%2Fsparql-results%2Bjson&timeout=0&debug=on";
         return query;
+    }
+    
+    public void getInfo(String query) throws Exception {
+        URL url = new URL(query);
+        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
     }
     
 }
